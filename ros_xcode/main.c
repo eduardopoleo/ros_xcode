7864612723dc2581d26a28f39dfd37bcbc3ef5b9
Eduardo Poleo <eduardopoleo@gmail.com>
2023-01-23 12:37:08 -0500

    Initial Commit

---
+++ ros_xcode/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ros_xcode
+//
+//  Created by Eduardo Poleo on 2023-01-23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
